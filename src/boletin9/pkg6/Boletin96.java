4ea64f677b7235da7f32a554e1bf228cf1ed7d15
propa <propa@DESKTOP-10ADTBO>
2017-06-20 15:31:43 +0200

    Acabado

---
+++ src/boletin9/pkg6/Boletin96.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9.pkg6;
+
+/**
+ *
+ * @author propa
+ */
+public class Boletin96 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Soldo soldos=new Soldo();
+       soldos.calcSoldo();
+    }
+    
+}
